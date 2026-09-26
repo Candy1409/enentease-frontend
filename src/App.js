7edf21d5213fdcc347d8e70adf32caaa1f6bdb25
chandana <314197574+Candy1409@users.noreply.github.com>
2026-09-26 10:08:48 +0530

    Add EventEase logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo from './logo-eventease.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="EventEase logo" style={{ height: '60px' }} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
